dbca030516cc1ec1fb976b27e1fbe6305710ce6c
Santiago <concursos.utn.frt@gmail.com>
2025-08-04 21:17:35 -0300

    agregamos funciones matematicas que suma y resta

---
+++ src/3-funcionesMatematica.js
@@ -0,0 +1,12 @@
+function sumar (num1, num2){
+    return num1 + num2 
+}
+
+function restar (num1, num2){
+    return num1 - num2 
+}
+
+module.exports ={
+    sumar,
+    restar
+}
---
+++ src/4-index.js
@@ -0,0 +1,3 @@
+const operaciones = require('./3-funcionesMatematica')
+console.log(operaciones.sumar(10,8))
+console.log(operaciones.restar(10,8))
