64c7f4301074543252ccb30b323e0c7889819a81
Santiago <concursos.utn.frt@gmail.com>
2025-08-04 21:34:01 -0300

    agregamos otrs funciones matematicas con export y import

---
+++ src/5-nuevasFuncionesMatematicas.mjs
@@ -0,0 +1,7 @@
+export function multiplicar (num1, num2){
+    return num1 * num2
+}
+
+export function dividir (num1, num2){
+    return num1 / num2
+}
---
+++ src/6-main.mjs
@@ -0,0 +1,3 @@
+import { multiplicar, dividir } from "./5-nuevasFuncionesMatematicas.mjs";
+console.log(multiplicar(5,4))
+console.log(dividir(24,3))
