4cb74d4b8cfdc278050f882ece2bc1721de09ea1
Santiago <concursos.utn.frt@gmail.com>
2025-08-06 19:57:32 -0300

    realizamos una prueba con modulos de terceros para cambiar el color del texto

---
+++ src/8-palabrasColores.js
@@ -0,0 +1,8 @@
+const colors = require('colors')
+// import colors from 'colors'
+const mensaje = 'Este es un mensaje con colores'
+console.log(mensaje)
+console.log(mensaje.bgMagenta)
+console.log(mensaje.green)
+console.log(mensaje.rainbow)
+console.log(mensaje.bgBlue.yellow)
